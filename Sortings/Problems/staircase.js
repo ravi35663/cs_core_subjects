7ae7b9761b250e408ba3c3592c2e0d049ee14731
ravi kumar <ravi.kumar07@telusinternational.com>
2024-11-22 12:50:22 +0530

    learning dsa

---
+++ Sortings/Problems/staircase.js
@@ -0,0 +1,33 @@
+/*
+Staircase Search:
+    You are given a M x N matrix which is row wise and column wise sorted. You have to search the index of a 
+    given integer K in the matrix.
+
+    Input Format:
+        Function contains integer M, integer N ,2D vector containing integers and an integer k.
+
+    Output Format:
+    Return a pair of integers {x,y} where x is the row index and y is column index of k in the matrix.
+    
+    Expected Complexity: Linear
+
+    Sample Testcase:
+    Input:
+    3 3
+
+    1 4 9
+    2 5 10
+    6 7 11
+
+    10
+
+    Output:
+    {1,2}
+    Explanation:
+    Index of 10 in matrix is 1,2.
+*/
+
+function Staircase(arr){
+
+}
+// const arr = [[1 4 9],[2 5 10],[6 7 11]];

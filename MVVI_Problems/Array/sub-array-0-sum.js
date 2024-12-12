d74e0ea14019f40140c01636eb18c66ac6e485e8
ravi kumar <ravi.kumar07@telusinternational.com>
2024-12-12 10:37:36 +0530

    learning dsa

---
+++ MVVI_Problems/Array/sub-array-0-sum.js
@@ -0,0 +1,24 @@
+/*
+    Subarray with 0 sum:
+
+    Difficulty: Medium  Accuracy: 39.79% Submissions: 283K+ Points: 4
+    
+    Given an array of integers, arr[]. Find if there is a subarray (of size at least one) with 0 sum. 
+    Return true/false depending upon whether there is a subarray present with 0-sum or not. 
+
+    Examples:
+        Input: arr[] = [4, 2, -3, 1, 6]
+        Output: true
+        Explanation: 2, -3, 1 is the subarray with a sum of 0.
+        
+        Input: arr = [4, 2, 0, 1, 6]
+        Output: true
+        Explanation: 0 is one of the element in the array so there exist a subarray with sum 0.
+        
+        Input: arr = [1, 2, -1]
+        Output: false
+    
+    Constraints:
+    1 <= arr.size <= 10^4
+    -10^5 <= arr[i] <= 10^5
+*/

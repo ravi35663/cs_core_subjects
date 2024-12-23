84ea9c80e34b4f2589567bf8ff5b53b494db1087
ravi <ravikr845430@gmail.com>
2024-12-23 07:50:19 +0530

    problem solving

--- MVVI_Problems/Array/sub-array-0-sum.js
+++ MVVI_Problems/Array/sub-array-0-sum.js
@@ -22,3 +22,55 @@
     1 <= arr.size <= 10^4
     -10^5 <= arr[i] <= 10^5
 */
+
+// Brute-Force: TC: O(N^3)
+// function subArraySum(arr,target){
+//     // let sub_arrays = [];
+//     let found_target_sum = false;
+//     for(let i=0;i<arr.length;i++){
+//         for(let j=i;j<arr.length;j++){
+//             let sum = 0;
+//             // let sub_arr = [];
+//             for(let k=i;k<j+1;k++){
+//                 sum +=arr[k];
+//                 sub_arr.push(arr[k]);
+//             }
+//             // sub_arrays.push(sub_arr);
+//             if(sum == target){
+//                 found_target_sum = true;
+//             }
+//         }
+//     }
+//     return found_target_sum;
+// }
+
+// TC: O(N^2)
+function subArraySum(arr,target){
+    const prefix_sum = [];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] == 0){
+            return true;
+        }
+    }
+    prefix_sum[0] = arr[0];
+    for(let i=1; i<arr.length; i++){
+        const sum = prefix_sum[i-1] + arr[0];
+        if(sum == 0){
+            return true;
+        }
+        prefix_sum[i] = sum;
+    }
+    for(let i=1;i<arr.length;i++){
+        for(let j=i;j<arr.length;j++){
+            if(prefix_sum[j] - prefix_sum[i-1] == 0){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+const arr = [4, 2, -3, 1, 6];
+console.log("Result is: ",subArraySum(arr,0));
--- test.js
+++ test.js
@@ -1,83 +1,101 @@
 /*
-    Rearrange Array Elements by Sign:
-    Given an array arr[] of size n, the task is to rearrange it in alternate positive and negative manner 
-    without changing the relative order of positive and negative numbers. 
-    In case of extra positive/negative numbers, they appear at the end of the array.
+    Subarray with 0 sum:
 
-    Note: The rearranged array should start with a positive number and 0 (zero) should be considered as a 
-    positive number.
+    Difficulty: Medium  Accuracy: 39.79% Submissions: 283K+ Points: 4
     
-    Examples: 
-    Input:  arr[] = {1, 2, 3, -4, -1, 4}
-    Output: arr[] = {1, -4, 2, -1, 3, 4}
+    Given an array of integers, arr[]. Find if there is a subarray (of size at least one) with 0 sum. 
+    Return true/false depending upon whether there is a subarray present with 0-sum or not. 
 
-
-    Input:  arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
-    Output: arr[] = {-5, 5, -2, 2, -8, 4, 7, 1, 8, 0}
-
+    Examples:
+        Input: arr[] = [4, 2, -3, 1, 6]
+        Output: true
+        Explanation: 2, -3, 1 is the subarray with a sum of 0.
+        
+        Input: arr = [4, 2, 0, 1, 6]
+        Output: true
+        Explanation: 0 is one of the element in the array so there exist a subarray with sum 0.
+        
+        Input: arr = [1, 2, -1]
+        Output: false
+    
+    Constraints:
+    1 <= arr.size <= 10^4
+    -10^5 <= arr[i] <= 10^5
 */
-//  Without using any extra space:
-function rearrangeArray(arr){
-    for(let i=0;i<arr.length;i=i+2){
-        let index = -1;
-        // To check next positive/negative number
-        for(let j=i+1;j<arr.length;j++){
-            if(arr[i] > 0 && arr[j]<0){
-                index = j;
-                break;
-            }else if(arr[i] < 0 && arr[j] > 0){
-                index = j;
-                break;
-            }
-        }
-        // To free space for the next positive/negative value
-        if(index == -1){
-            return arr;
-        }else{
-            const val = arr[index];
-            for(let j=index;j>i+1;j--){
-                [arr[j],arr[j-1]] = [arr[j-1],arr[j]]
-            }
-            arr[i+1] = val;
-        }
-    }
-    return arr;
-}
 
-// By using extra space: T.C: O(N) & S.C: O(N)
-function rearrangeArrayElements(arr){
-    const pos = [];
-    const neg = [];
+// Brute-Force: TC: O(N^3)
+// function subArraySum(arr,target){
+//     // let sub_arrays = [];
+//     let found_target_sum = false;
+//     for(let i=0;i<arr.length;i++){
+//         for(let j=i;j<arr.length;j++){
+//             let sum = 0;
+//             // let sub_arr = [];
+//             for(let k=i;k<j+1;k++){
+//                 sum +=arr[k];
+//                 sub_arr.push(arr[k]);
+//             }
+//             // sub_arrays.push(sub_arr);
+//             if(sum == target){
+//                 found_target_sum = true;
+//             }
+//         }
+//     }
+//     return found_target_sum;
+// }
+
+// TC: O(N^2)
+function subArraySum(arr,target){
+    const prefix_sum = [];
     for(let i=0;i<arr.length;i++){
-        if(arr[i] >= 0){
-            pos.push(arr[i]);
-        }else{
-            neg.push(arr[i]);
+        if(arr[i] == 0){
+            return true;
         }
     }
-    if(arr[0] > 0){
-        return mergePosNegArray(pos,neg);
-    }else{
-        return mergePosNegArray(neg,pos);
+    prefix_sum[0] = arr[0];
+    for(let i=1; i<arr.length; i++){
+        const sum = prefix_sum[i-1] + arr[i];
+        if(sum == 0){
+            return true;
+        }
+        prefix_sum[i] = sum;
     }
+    for(let i=1;i<arr.length;i++){
+        for(let j=i;j<arr.length;j++){
+            if(prefix_sum[j] - prefix_sum[i-1] == 0){
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-function mergePosNegArray(arr1,arr2){
-    let i=0,j=0;
-    const result = [];
-    while(i<arr1.length && j<arr2.length){
-        result.push(arr1[i++]);
-        result.push(arr2[j++]);
+// More Optimization : TC: O(N)
+function subArraySum(arr,target){
+    const prefix_sum = [];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] == 0){
+            return true;
+        }
     }
-    while(i<arr1.length){
-        result.push(arr1[i++]);
+    prefix_sum[0] = arr[0];
+    for(let i=1; i<arr.length; i++){
+        const sum = prefix_sum[i-1] + arr[i];
+        if(sum == 0){
+            return true;
+        }
+        prefix_sum[i] = sum;
     }
-    while(j<arr2.length){
-        result.push(arr2[j++]);
+    for(let i=1;i<arr.length;i++){
+        for(let j=i;j<arr.length;j++){
+            if(prefix_sum[j] - prefix_sum[i-1] == 0){
+                return true;
+            }
+        }
     }
-    return result;
+    return false;
 }
 
-// const arr = [1, 2, 3, -4, -1, 4];
-const arr = [-5, -2, 5, 2, 4, 7, 1, 8, 0, -8];
-console.log("Result is: ",rearrangeArrayElements(arr));
+
+const arr = [4, 2, -3, 1, 6];
+console.log("Result is: ",subArraySum(arr,0));

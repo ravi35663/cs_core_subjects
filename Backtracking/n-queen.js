b33030bb3146e46d02ae66020777cc20e9543c02
ravi kumar <ravi.kumar07@telusinternational.com>
2025-02-27 10:23:27 +0530

    learning backtracking

---
+++ Backtracking/n-queen.js
@@ -0,0 +1,25 @@
+/*
+==> N-Queen Problems 👑:
+        N-Queen is the problem of placing N chess queens on an N×N chessboard so that no two queens 
+        attack each other.
+
+        Input
+            N = 4
+
+        Output:
+            [ 
+                [ 0, 1, 0, 0 ], 
+                [ 0, 0, 0, 1 ], 
+                [ 1, 0, 0, 0 ], 
+                [ 0, 0, 1, 0 ] 
+            ]
+            // Here 1 is the place of queen
+*/ 
+/*
+==> Problem Variations:
+    1) Print one configuration
+    2) Print all configuration
+    3) Count possible configuration
+*/ 
+
+

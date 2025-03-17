b6e49f4f1bf517fc2e42ebfb5f5a95c72658f710
ravi kumar <ravi.kumar07@telusinternational.com>
2025-03-17 21:50:45 +0530

    learning linked list

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -5,3 +5,22 @@
     -> To get any item, we have to traverse it from the beginning .
     -> Insertion and deletion is very fast in the linked list compare to array.
 */
+
+/*
+==> LinkedList VS Array:
+    -> In array memory are continuos 
+    -> In linkedList memory created on the fly (Dynamically).
+    -> Linked list and array are custom data type
+    -> There is no access of index in linked
+    -> There is no direct access of an elements in linkedList
+*/
+
+/*
+==> Basic operation on linkedList:
+    1) Insertion: (start,end,anywhere)
+    2) Deletion: (start,end,anywhere)
+    3) Searching: (Iteration, recursively)
+    4) Creation of linked list: 
+    5) Deletion of linked list
+    6) Iteration of linked list
+*/

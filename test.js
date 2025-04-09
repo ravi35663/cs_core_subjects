3da2c02a73f00ddc9ca7f7d8df474c880a11d328
ravi <ravikr845430@gmail.com>
2025-04-09 21:52:20 +0530

    learning dsa

---
+++ test.js
@@ -0,0 +1,13 @@
+
+/*
+    Minimum Difference
+    Implement a function that takes in two non-empty arrays of integers, finds the pair of numbers 
+    (one from each array) who absolute difference is closest to zero, and returns a pair containing 
+    these two numbers, with the first number from array. Only one such pair will exist for the test.
+
+    Input:
+        Array1 = [23, 5, 10, 17, 30]
+        Array2 = [26, 134, 135, 14, 19]
+    Output:
+      17,19
+*/

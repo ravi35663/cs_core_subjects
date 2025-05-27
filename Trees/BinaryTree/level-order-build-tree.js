717c299df7c413fd8f6ffb90ef0dadbba31bae7d
ravi kumar <ravi.kumar07@telusinternational.com>
2025-05-27 18:34:03 +0530

    learning dsa

---
+++ Trees/BinaryTree/level-order-build-tree.js
@@ -0,0 +1,15 @@
+/*
+   ==> Build a binary-tree from level order input, -1 input represent null:
+   
+   Input:   
+   const arr = [1,2,3,4,5,-1,6,-1,-1,7,-1,-1,-1,-1,-1,-1]
+   Output:
+         (1)
+        /   \
+      (2)   (3)
+      /  \     \
+    (4)   (5)  (6)
+         /
+       (7)
+
+*/

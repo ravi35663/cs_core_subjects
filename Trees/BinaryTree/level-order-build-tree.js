4cad3ec00ead73ba4aeef73128215fb201b15958
ravi kumar <ravi.kumar07@telusinternational.com>
2025-05-28 18:48:26 +0530

    learning dsa

--- Trees/BinaryTree/level-order-build-tree.js
+++ Trees/BinaryTree/level-order-build-tree.js
@@ -1,3 +1,5 @@
+const Queue = require("../../Queue/queue-with-array");
+
 /*
    ==> Build a binary-tree from level order input, -1 input represent null:
    
@@ -11,5 +13,71 @@
     (4)   (5)  (6)
          /
        (7)
-
 */
+class Node{
+  constructor(val){
+    this.left = null;
+    this.right = null;
+    this.value = val;
+  }
+}
+
+
+// const Queue = require("../../Queue/queue-with-array");
+
+
+function levelOrderBuild(arr){
+    if(arr[0] == -1) return null;
+    let index = 0;
+    const root = new Node(arr[index++]);
+    const q = new Queue(100);
+    q.push(root);
+
+    while(q.cs != 0 && index < arr.length){
+      const item = q.viewFront();
+      q.pop();
+
+      const leftVal = arr[index++];
+      const rightVal = arr[index++];
+
+      if(leftVal != -1){
+        item.left = new Node(leftVal);
+        q.push(item.left);
+      }
+
+      if(rightVal != -1){
+        item.right = new Node(rightVal);
+        q.push(item.right)
+      }
+    }
+    return root
+}
+
+// Level Order Traversal:
+function levelOrderTraversal(root){
+    const q = new Queue(100);
+    q.push(root);
+    q.push(null);
+    while(q.cs !=0 ){
+        const item = q.viewFront();
+        q.pop();
+        if(item){
+            process.stdout.write(`${item.value} `)
+            if(item.left){
+                q.push(item.left)
+            }
+            if(item.right){
+                q.push(item.right)
+            }
+        }else{
+            console.log();
+            if(q.cs != 0){
+                q.push(null)
+            }
+        }
+    }
+}
+const arr = [1,2,3,4,5,-1,6,-1,-1,7,-1,-1,-1,-1,-1,-1]
+const root = levelOrderBuild(arr);
+levelOrderTraversal(root);
+// console.log("Root is: ",root);
--- test.js
+++ test.js
@@ -1,89 +1,83 @@
-//
+const Queue = require("./Queue/queue-with-array");
+
+/*
+   ==> Build a binary-tree from level order input, -1 input represent null:
+   
+   Input:   
+   const arr = [1,2,3,4,5,-1,6,-1,-1,7,-1,-1,-1,-1,-1,-1]
+   Output:
+         (1)
+        /   \
+      (2)   (3)
+      /  \     \
+    (4)   (5)  (6)
+         /
+       (7)
+*/
 class Node{
-    constructor(value){
-        this.left = null;
-        this.right = null;
-        this.value = value
-    }
+  constructor(val){
+    this.left = null;
+    this.right = null;
+    this.value = val;
+  }
 }
 
-function buildBinaryTree(arr){
-    let index  = 0;
-    function tree(){
-        if(arr[index] == null || arr[index] == -1 || arr[index] == undefined){
-            index++;
-            return null;
-        }
-        const node = new Node(arr[index++])
-        node.left = tree();
-        node.right = tree();
-        return node;
+
+// const Queue = require("../../Queue/queue-with-array");
+
+
+function levelOrderBuild(arr){
+    if(arr[0] == -1) return null;
+    let index = 0;
+    const root = new Node(arr[index++]);
+    const q = new Queue(100);
+    q.push(root);
+
+    while(q.cs != 0 && index < arr.length){
+      const item = q.viewFront();
+      q.pop();
+
+      const leftVal = arr[index++];
+      const rightVal = arr[index++];
+
+      if(leftVal != -1){
+        item.left = new Node(leftVal);
+        q.push(item.left);
+      }
+
+      if(rightVal != -1){
+        item.right = new Node(rightVal);
+        q.push(item.right)
+      }
     }
-    return tree()
-}
-//Binary Tree Traversal:
-// PreOrderTraversal: Root, Left, Right
-function PreOrderTraversal(root){
-    if(!root){
-        return;
-    }
-    process.stdout.write(`${root.value} `)
-    PreOrderTraversal(root.left);
-    PreOrderTraversal(root.right);
-}
-// InOrderTraversal: Left, Root, Right
-function InOrderTraversal(root){
-    if(!root){
-        return;
-    }
-    InOrderTraversal(root.left)
-    process.stdout.write(`${root.value} `)
-    InOrderTraversal(root.right)
-}
-// PostOrderTraversal: Left, Right, Root:
-function PostOrderTraversal(root){
-    if(!root){
-        return;
-    }
-    PostOrderTraversal(root.left)
-    PostOrderTraversal(root.right)
-    process.stdout.write(`${root.value} `)
+    return root
 }
 
-// LevelOrderTraversal:
-const Queue = require('./Queue/queue-with-array')
-function LevelOrderTraversal(root){
-    if(!root) return;
-    let q = new Queue(100);
-    q.push(root)
-    q.push(null)
-    while(q.cs != 0){
-        const f = q.viewFront();
+// Level Order Traversal:
+function levelOrderTraversal(root){
+    const q = new Queue(100);
+    q.push(root);
+    q.push(null);
+    while(q.cs !=0 ){
+        const item = q.viewFront();
         q.pop();
-        if(f){
-            process.stdout.write(`${f.value} `)
-            if(f.left){
-                q.push(f.left)
+        if(item){
+            process.stdout.write(`${item.value} `)
+            if(item.left){
+                q.push(item.left)
             }
-            if(f.right){
-                q.push(f.right)
+            if(item.right){
+                q.push(item.right)
             }
         }else{
-            console.log()
-            if(q.cs !=0 ){
+            console.log();
+            if(q.cs != 0){
                 q.push(null)
             }
         }
     }
 }
-
-const arr = [1,2,4,-1,-1,5,7,-1,-1,-1,3,-1,6,-1,-1]
-const root = buildBinaryTree(arr);
-console.log("--------------------Preorder-traversal--------------------------")
-PreOrderTraversal(root); //1 2 4 5 7 3 6
-console.log("--------------------Inorder-traversal--------------------------")
-InOrderTraversal(root) // 4 2 7 5 1 3 6
-console.log("--------------------Postorder-traversal--------------------------")
-PostOrderTraversal(root); // 4 7 5 2 6 3 1
-console.log("--------------------Level-Order-traversal--------------------------")
-LevelOrderTraversal(root);
+const arr = [1,2,3,4,5,-1,6,-1,-1,7,-1,-1,-1,-1,-1,-1]
+const root = levelOrderBuild(arr);
+levelOrderTraversal(root);
+// console.log("Root is: ",root);

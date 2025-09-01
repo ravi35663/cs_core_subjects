e3d21ad0ddd45de739fee0954655b599934e6d3a
ravi <ravikr845430@gmail.com>
2025-09-01 19:36:30 +0530

    learning dsa

--- test.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Example of Head Recursion
-function headRecursion(n) {
-    if (n > 0) {
-        headRecursion(n - 1);  // Recursive call before processing
-        console.log(n + " ");  // Processing after recursion
-    }
-}
-headRecursion(5); // 1 2 3 4 5

0c83add2f3662d3e837d2afe141d96f6f9122154
ravi <ravikr845430@gmail.com>
2026-01-14 21:39:21 +0530

    learning dsa

--- test.js
+++ test.js
@@ -1,14 +1,17 @@
-arr = [-2,3,4,-1,5,-12,6,1,3,2]
-function maxSubArraySum(arr){
-    let sum = 0;
-    let max = -Infinity;
-    for(let item of arr){
-        sum +=item;
-        if(sum > max){
-            max = sum;
-        }
-        if(sum < 0){
-            sum = 0;
-        }
-    }
+/*
+    Counting sort:
+    -> if elements in the array in the range then use counting sort
+    -> Example:
+        arr = [5,3,4,1,2,1,2]
+        here max  =  5;
+        freq_arr = [0,2,2,1,1,1,0,0] ==> count of each v
+        then arr = [1,1,2,2,3,4,5]
+*/
+
+
+function countingSort(arr){
+    let 
 }
+const arr = [5,3,4,1,2,33,50]
+const result = countingSort(arr)
+console.log("Sorted array is : ",result);

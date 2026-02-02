b3bf7ca4441221eb19b7a1429cef875a176a20ec
ravi <ravikr845430@gmail.com>
2026-02-02 08:02:52 +0530

    learning binary tree

--- 12.binary-tree/index.js
+++ 12.binary-tree/index.js
@@ -1,4 +1,9 @@
 /*
+=>  Hierarchical Data Structure:
+    -   A Hierarchical Data Structure organizes data in a tree-like form, where 
+        each item (node) has one parent (except the root) and zero or more children.
+*/
+/*
 ===> Tree :
     -> Tree is a data structure that consists of nodes in a parent/child relationship.
     -> List: Lists are linear data structures
@@ -17,6 +22,7 @@
     -> Descendant: Below nodes of a particular node.
     -> Height: Total depth of tree
     -> Depth of a node: 
+    ->  Node: anything stores the data is node.
 
 ===> Uses of tree in computer science:-
     -> HTML DOM: (Nested elements)
@@ -33,6 +39,7 @@
     -> Binary Trees: Can have 0, 1 or 2 children. But cannot have more than 2 children.
     -> Binary Search Trees: Binary tree + children are sorted in a particular way.
         ..etc
+    -> n-ary Tree
 */
 
 /*
@@ -43,3 +50,28 @@
         calls on each sub-trees iteratively.
 */
 
+/*
+=> Types of Binary Tree:
+    1) Full BT:     All node have must 2 or 0 children
+    2) Complete BT: 
+        Conditions:
+            1) All levels must be completely filled except the last level: (2^level : 0<=level<=n-1)
+            2) The last level has all nodes in left as possible, must node the gap level > 1
+    3) Perfect BT:
+            Every Level is completely filled:
+        OR,
+            1) All the leaf nodes are at the same level.
+            2) All non-leaf nodes must have 2 children
+    4) Balanced BT:
+        -   Height difference between left and right sub-tree at any node must be at max 1.
+    5) Degenerate BT:
+        -   Every node has a single child
+*/
+
+class Node{
+    constructor(value){
+        this.left = null;
+        this.data = value;
+        this.right = null;
+    }
+}

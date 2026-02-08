c6ddcf6dbd905668b1b8b15e5aec35f869d22d69
ravi <ravikr845430@gmail.com>
2026-02-08 20:33:08 +0530

    learning dsa

--- 12.binary-tree/2.tree-traversal/1.dfs/index.js
+++ 12.binary-tree/2.tree-traversal/1.dfs/index.js
@@ -5,7 +5,7 @@
     ->  There are three common variations of DFS when applied to binary trees, which 
         determine the order in which nodes are visited. 
         These variations are:
-        1) Pre-Order, 
-        2) In-Order, 
-        3) Post-Order traversals:
+        1) Pre-Order: Root, Left, right
+        2) In-Order: Left, Root, Right
+        3) Post-Order traversals: Left, Right, Root
 */

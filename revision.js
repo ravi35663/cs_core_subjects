b38e6c3a87a9dadbc085e75774589a69fcd73326
Ravi Kumar <ravikr@Ravis-MacBook-Pro.local>
2026-07-15 12:42:14 +0530

    learning system design

--- revision.js
+++ revision.js
@@ -1,4 +1,15 @@
 /*
-==> Current:
-1) System design
+=> Revision Cycle:
+A) System Design:
+    1) OOP: 
+        -   Introduction                -   Done
+        -   oop-core-principles         -   Done
+        -   Adv Oop feature             -   Done
+        -   Relationship                -   Done
+        -   Adv-pro-oop                 -   Done
+        -   Solid                       -   Done
+
+    2) LLD:
+        -   Introduction                 -   Done
+        -   SOLID Principle              -   Done
 */
